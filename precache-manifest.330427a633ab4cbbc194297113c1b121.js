6fc4f57c7c38f5e4ad2e196bb3c25b2584d0ae9f
Munkhjin Enkhbat <fa16b489@ufe.edu.mn>
2020-08-20 21:01:45 +0800

    Updates

---
+++ precache-manifest.330427a633ab4cbbc194297113c1b121.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "81d165888e97e13b752e",
+    "url": "/web/static/css/main.29a8d17b.chunk.css"
+  },
+  {
+    "revision": "81d165888e97e13b752e",
+    "url": "/web/static/js/main.81d16588.chunk.js"
+  },
+  {
+    "revision": "83a65db5781aa0ee448c",
+    "url": "/web/static/css/1.0abd3961.chunk.css"
+  },
+  {
+    "revision": "83a65db5781aa0ee448c",
+    "url": "/web/static/js/1.83a65db5.chunk.js"
+  },
+  {
+    "revision": "05773d53a80748feb222",
+    "url": "/web/static/js/runtime~main.05773d53.js"
+  },
+  {
+    "revision": "b2f75fe75de16097c8b70afbe044fa7a",
+    "url": "/web/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/emunkhjin/emunkhjin.github.io.git/precache-manifest.32f277de0817817e4a23a375f4154618.js"
+  "/web/precache-manifest.330427a633ab4cbbc194297113c1b121.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/emunkhjin/emunkhjin.github.io.git/index.html", {
+workbox.routing.registerNavigationRoute("/web/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
